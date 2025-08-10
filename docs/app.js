fe415ce8ae80e65fd0cb284872106415b1bb0060
eirikreiestad <eirik@reiestad.no>
2025-08-10 21:35:42 +0200

    fix: changed . to /

--- docs/app.js
+++ docs/app.js
@@ -27,7 +27,7 @@ function excelDateToNorwegian(serial) {
     const day = String(date_info.getUTCDate()).padStart(2, '0')
     const month = String(date_info.getUTCMonth() + 1).padStart(2, '0')
     const year = date_info.getUTCFullYear()
-    return `${day}.${month}.${year}`
+    return `${day}/${month}/${year}`
 }
 
 // Trim column headers in Excel data
